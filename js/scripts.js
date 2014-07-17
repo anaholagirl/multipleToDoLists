6c08138576a4ad128d2ff0409b3a25b195e40d93
Cindy Ward & Linda Gibson <student@epicodus.com>
2014-07-17 14:56:39 -0700

    Completed multiple to do lists assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,40 @@
 $(document).ready (function () {
-    $("form#new-task").submit (function () {
-        var inputTaskName = $("input#new-task-name").val();
-        var inputTaskOwner = $("input#new-task-owner").val();
-        var inputCompleteMonth = $("select#new-complete-month").val();
-        var inputCompleteDay = $("select#new-complete-day").val();
-        var inputPriority = $("select#new-priority").val();
-        var isTaskDone = false;
-        var taskText = "No";
-        var newTask = {taskName: inputTaskName, taskOwner: inputTaskOwner,
-                    taskCompleteMonth: inputCompleteMonth,
-                    taskCompleteDay: inputCompleteDay,
-                    taskPriority: inputPriority,
-                    taskDone: isTaskDone};
 
-        $("ul#tasks").append("<li><input type='checkbox' id='task-done' value='0'><span class='task'>" + newTask.taskName + "</span></li>");
+    var currentList;
 
-        $("input#task-done").click(function() {
-            if ($(this).is(":checked")) { // the checkbox was checked
-                newTask.taskDone = true;
-            } else { // the checkbox was unchecked
-                newTask.taskDone = false;
-            };
-            $(".task-complete").text(taskText);
+    $("form#new-list").submit (function (event) {
+        var inputListName = $("input#new-list-name").val();
+        var newList = {listName: inputListName, taskList: [] };
+        currentList = newList;
+
+        $("ul#lists").append("<li><span class='list'>" + newList.listName + "</span></li>");
+        $("input#new-list-name").val("");
+
+
+       $(".list").last().click(function () {
+            $("#show-list").show();
+            $("#show-list h2").text(newList.listName);
+            $("#show-list ul").text("");
+            newList.taskList.forEach (function (eachTask) {
+                $("ul#tasks").append("<li><span class='task'>" + eachTask.taskName + "</span></li>");
+             });
+            currentList = newList;
         });
 
-        $("input#new-task-name").val("");
-        $("input#new-task-owner").val("");
-        $("select#new-complete-month").val("July");
-        $("select#new-complete-day").val("16");
-
-        $(".task").last().click(function () {
-            $("#show-task").show();
-            $("#show-task h2").text(newTask.taskName);
-            $(".task-name").text(newTask.taskName);
-            $(".task-owner").text(newTask.taskOwner);
-            $(".task-complete-date").text(newTask.taskCompleteMonth + " " +
-                                          newTask.taskCompleteDay);
-            $(".task-priority").text(newTask.taskPriority);
-            taskText = "No";
-            if (newTask.taskDone) {
-                taskText = "Yes"
-            } else {
-                taskText = "No";
-            };
-            $(".task-complete").text(taskText);
-
-        });
-
-    event.preventDefault();
+       event.preventDefault();
 
     });
+
+    $("form#new-task").submit (function (event) {
+        var taskItem = {taskName: ""};
+        var inputTaskName = $("input#new-task-name").val();
+        taskItem.taskName = inputTaskName;
+        currentList.taskList.push(taskItem);
+        $("ul#tasks").append("<li><span class='task'>" + currentList.taskList[currentList.taskList.length-1].taskName + "</span></li>");
+        $("input#new-task-name").val("");
+
+        event.preventDefault();
+
+    });
+
 });
